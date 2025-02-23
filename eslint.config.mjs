c5c7ff2182cffb0386ccd1e195f6f80632c85051
aargon007 <muhai4959@gmail.com>
2025-02-23 21:31:19 +0600

    added neccesary packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+       "@next/next/no-img-element": "off",
+        "react/no-unescaped-entities": 0,
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-unused-expressions": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
