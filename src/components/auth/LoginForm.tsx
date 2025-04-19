c7a1ab95ce1a92e7f45e5f18f4fe3a7b723f873c
aargon007 <muhai4959@gmail.com>
2025-04-19 22:15:20 +0600

    login integrated

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,16 +1,42 @@
 "use client"
 
+import { FormEvent, useState } from "react"
 import Link from "next/link";
-import { useState } from "react"
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 import { LuCheck, LuLock, LuMail } from "react-icons/lu"
+import { loginUser } from "@/services/user.service";
+import Spinner from "../ui/Spinner";
+import { cn } from "@/utils/cn";
 
 const LoginForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [rememberMe, setRememberMe] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const form = e.target as HTMLFormElement;
+        const formDataObj = new FormData(form);
+
+        try {
+            const response = await loginUser(formDataObj);
+            if (response.success) {
+                router.push("/chat");
+            }
+            toast.success(response.message);
+        } catch (error) {
+            toast.error("Error logging in");
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <div>
                 <div className="relative">
                     <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
@@ -18,10 +44,13 @@ const LoginForm = () => {
                     </div>
                     <input
                         type="email"
+                        name="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         placeholder="example@mail.com"
                         className="inputField"
+                        autoComplete="off"
+                        required
                     />
                 </div>
             </div>
@@ -33,10 +62,12 @@ const LoginForm = () => {
                     </div>
                     <input
                         type="password"
+                        name="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="password"
                         className="inputField"
+                        required
                     />
                 </div>
             </div>
@@ -66,9 +97,13 @@ const LoginForm = () => {
 
             <button
                 type="submit"
-                className="primaryButton"
+                className={cn(
+                    "primaryButton",
+                    isLoading && "pointer-events-none opacity-50",
+                    (!rememberMe || !email || !password) && "pointer-events-none opacity-50"
+                )}
             >
-                Log in
+                {isLoading ? <Spinner /> : "Log in"}
             </button>
         </form>
     );
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { hash } from "bcrypt"
+import { hash, compare } from "bcrypt"
 import { cookies } from 'next/headers'
 import prisma from "@/lib/prisma"
 import { config } from "@/lib/config"
@@ -55,6 +55,58 @@ export async function createUser(formData: FormData): Promise<TResponse> {
 
     } catch (error) {
         console.error("User creation failed:", error)
+        return {
+            success: false,
+            message: "Something went wrong. Please try again later.",
+        }
+    }
+}
+
+
+// login
+export async function loginUser(formData: FormData): Promise<TResponse> {
+    try {
+        const email = formData.get("email") as string
+        const password = formData.get("password") as string
+
+        const user = await prisma.user.findUnique({ where: { email } })
+        if (!user) {
+            return {
+                success: false,
+                message: "User not found.",
+            }
+        }
+
+        const isPasswordValid = await compare(password, user.password);
+        if (!isPasswordValid) {
+            return {
+                success: false,
+                message: "Invalid password.",
+            }
+        }
+
+        const token = createToken({
+            id: user.id,
+            email: user.email,
+            name: `${user.first_name} ${user.last_name ?? ""}`.trim(),
+        });
+
+        const cookieStore = await cookies();
+        cookieStore.set("aargon", token, {
+            httpOnly: true,
+            maxAge: config.expires_in, // 30 days
+            path: "/",
+            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        return {
+            success: true,
+            message: "Login successful.",
+        };
+
+    } catch (error) {
+        console.error("Login failed:", error)
         return {
             success: false,
             message: "Something went wrong. Please try again later.",
